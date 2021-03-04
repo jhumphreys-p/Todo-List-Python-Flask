ad18d2f7b0eb080eff2773d7f700b739eb01f0c9
jhumphreys-p <jhumphreysp@hotmail.com>
2021-03-04 17:41:53 +0000

    TodoList

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Task
 #from models import Person
 
 app = Flask(__name__)
@@ -30,16 +30,81 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/user/<name>', methods=['GET'])
+def get_user_information(name):
+    user = User.get_by_name(name)
+    if user:
+        return jsonify(user.serialize()), 200
+    else: "User nor found", 404
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/user', methods=['POST'])
+def create_user():
+    name = request.get_json().get("name", None)
+    if name:
+        user = User(name=name, is_active=True)
+        user.add()
+        return "Created user", 201
+    return "Missing new user name", 400
 
-    return jsonify(response_body), 200
+@app.route('/user/<name>', methods=['PUT'])
+def update_user(name):
+    user = User.get_by_name(name)
+    new_name = request.get_json().get("name", None)
+    
+    if new_name and User.get_by_name(new_name) is None:
+        updated_user = user.update_name(new_name)
+        return updated_user.serialize(), 200
+    
+    return "Another user with this username already exists. Maybe it's your evil twin. Spooky!", 400
+
+@app.route('/user/<name>', methods=['DELETE'])
+def delete_user(name):
+    user = User.get_by_name(name)
+    if user:
+        delete_user = User.delete_user(user)
+        return f'Your account has been deleted: {user}', 200
+    else:
+        return 'That username does not exists', 404
+    
+@app.route('/user/<name>/tasks', methods=['GET'])
+def get_user_tasks(name):
+    user = User.get_by_name(name)
+    tasks = Task.get_by_user(user.id)
+    tasks = [task.serialize() for task in tasks if task is not None]
+    return jsonify(tasks), 200
+
+@app.route('/user/<name>/tasks', methods=['POST'])
+def add_user_new_task(name):
+    user = User.get_by_name(name)
+    new_tasks = request.get_json()
+
+    for new_task in new_tasks:
+        task = Task(
+            label= new_task.get('label'),
+            is_done= new_task.get('is_done'),
+            user_id=user.id
+        )
+        task.add(name)
+
+    return "Added tasks",200
+
+@app.route('/user/<name>/tasks/<id>', methods=['PUT'])
+def update_task(name, id):
+    body = request.get_json()
+    id = int(id)
+    task = Task.get_by_id(id)
+    task.update_task(body['label'], body['is_done'], )
+
+    return "Task Updated Successfully", 200
+
+@app.route('/user/<name>/tasks/<id_tasks>', methods=['DELETE'])
+def delete_tasks(name, id_tasks):
+    task = Task.get_task(id_tasks)
+    task.delete()
+    return f'Your task has been deleted', 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+    
--- src/models.py
+++ src/models.py
@@ -1,19 +1,86 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Table, Column, Integer, ForeignKey, String, DateTime, Date, Time
+from sqlalchemy.orm import relationship, backref
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__="user"
+
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    name = db.Column(db.String(250), unique=True, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    task = db.relationship('Task', lazy=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'User {self.name}'
 
     def serialize(self):
         return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
         }
+    
+    @classmethod
+    def get_by_name(cls, name):
+        user= cls.query.filter_by(name = name).first()
+        return user
+
+    def add(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update_name(self, name):
+        self.name = name
+        db.session.commit()
+        return self
+
+    def delete_user(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Task(db.Model):
+    __tablename__="tasks"
+
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(250), nullable=False)
+    is_done = db.Column(db.Boolean(), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    def __repr__(self):
+        return f'The task {self.label} has the status {self.is_done}'
+        
+    def serialize(self):
+        return {
+            "id": self.user_id,
+            "label": self.label,
+            "is_done": self.is_done,
+        }
+
+    @classmethod
+    def get_by_user(cls, user):
+        tasks= cls.query.filter_by(user_id=user)
+        return tasks
+
+    @classmethod
+    def get_by_id(cls, task_id):
+        task = cls.query.filter_by(id = task_id).first()
+        return task
+
+    @classmethod
+    def get_task(cls, id_tasks):
+        tasks= cls.query.get(id_tasks)
+        return tasks
+
+    def add(self, user):
+        db.session.add(self)
+        db.session.commit()
+
+    def update_task(self, new_label, new_done):
+        self.label = new_label
+        self.is_done = new_done
+        db.session.commit()
+        return self
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
